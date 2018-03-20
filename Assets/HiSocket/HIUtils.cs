2d97ad91c40c4f11afd4fc1481e559f990c197e3
alex <alex@bais.com.tw>
2018-03-20 13:17:54 +0800

    add hisocket-unity

---
+++ Assets/HiSocket/HIUtils.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HiSocket
+{
+    public class HIUtils
+    {
+        public HIUtils()
+        {
+        }
+
+        public static byte[] ToByteArray(object source)
+        {
+            var Formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            using (var stream = new System.IO.MemoryStream())
+            {
+                Formatter.Serialize(stream, source);
+                return stream.ToArray();
+            }
+        }
+
+        public static object ToObject(byte[] source)
+        {
+            var Formatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+            using (var stream = new System.IO.MemoryStream(source))
+            {
+                return Formatter.Deserialize(stream);
+            }
+        }
+
+        //1byte(ID) + 1byte(chann) + byte(data)
+        public static byte[] JoinHeaderBytes(byte ID , byte chann , byte[] data){
+            byte[] newArray = new byte[data.Length + 2];
+            data.CopyTo(newArray, 2);
+            newArray[0] = ID;
+            newArray[1] = chann;
+            return newArray;
+        }
+
+        //1byte(ID) + 1byte(chann) + byte(data)
+        public static byte[] SplitHeaderBytes(byte[] data){
+            var newArray = new byte[data.Length - 2];
+            Array.Copy(data, 2, newArray, 0, data.Length - 2);
+            return newArray;
+        }
+
+    }
+}
---
+++ Assets/Test.cs
@@ -0,0 +1,109 @@
+﻿//****************************************************************************
+// Description: only use tcp socket to send and receive message
+// Should achieve pack and unpack logic by yourself
+// Author: hiramtan@live.com
+//****************************************************************************
+
+using HiSocket;
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+[Serializable]
+class MemberInfo
+{
+    public int ID = 0;
+    public string Name = "";
+    public bool IsEnable = false;
+    //public List<string> LicenseList = new List<string>();
+}
+
+[Serializable]
+class DictMemberInfo
+{
+    public Dictionary<string, MemberInfo> mem = new Dictionary<string, MemberInfo>();
+}
+
+public class Test : MonoBehaviour
+{
+    //private ITcp _tcp;
+    private TcpConnection _tcp;
+
+    //private IPackage _packer = new Packer();
+    // Use this for initialization
+    void Start()
+    {
+        //_tcp = new TcpConnection(_packer);
+        _tcp = TcpConnection.getInstance();
+        _tcp.StateChangeEvent += OnState;
+        _tcp.ReceiveEvent += OnReceive;
+        Connect();
+        _tcp.TreadRun();
+        Send();
+    }
+
+    void Connect()
+    {
+        _tcp.Connect("127.0.0.1", 7777);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //_tcp.Run();
+    }
+
+    void OnState(SocketState state)
+    {
+        //Debug.Log("current state is: " + state);
+        if (state == SocketState.Connected)
+        { 
+            Debug.Log("socket Connected");
+        }
+
+        if (state == SocketState.Connecting){
+            Debug.Log("socket Conneting");
+        }
+
+        if (state == SocketState.DisConnected){
+            Debug.Log("socket DisConnected");
+        }
+    }
+
+    void Send()
+    {
+        HINetworkData hinfo = new HINetworkData();
+        HINetworkItem item = new HINetworkItem();
+        item.ID = 123;
+        item.Name = "myalex";
+        hinfo.ndada.Add("objname1", item);
+
+        //物件轉位元組陣列
+        byte[] MemberABytes = HIUtils.ToByteArray(hinfo);
+        byte[] data = HIUtils.JoinHeaderBytes(0x11, 0x22, MemberABytes);
+        _tcp.Send( data );
+    }
+
+    private void OnApplicationQuit()
+    {
+        if (_tcp.IsConnected)
+            _tcp.DisConnect();
+    }
+
+    void OnReceive(byte[] bytes)
+    {
+        //Debug.Log("receive bytes: " + bytes.Length);
+        //string msg = Encoding.Unicode.GetString(bytes, 0,bytes.Length);
+        Debug.Log("OnReceive");
+        //ID
+        Debug.Log(bytes[0].ToString("X2")); 
+        //chann
+        Debug.Log(bytes[1].ToString("X2"));
+
+        byte[] data = HIUtils.SplitHeaderBytes(bytes);
+        HINetworkData info = (HINetworkData)HIUtils.ToObject(data);
+        Debug.Log( info.ndada["objname1"].Name );
+    }
+
+}
