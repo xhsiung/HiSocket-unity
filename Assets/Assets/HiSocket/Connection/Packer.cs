5ed97e9068247495bb431da318e1f44a038f4193
alex <alex@bais.com.tw>
2018-03-21 16:53:15 +0800

    fix

---
+++ Assets/Assets/HiSocket/Connection/Packer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HiSocket
+{
+    public class Packer : IPackage
+    {
+        public void Unpack(IByteArray reader, Queue<byte[]> receiveQueue)
+        {
+            //get head length or id
+            //while (reader.Length >= 1)
+            //{
+            //    byte bodyLength = reader.Read(1)[0];
+
+            //    if (reader.Length >= bodyLength)
+            //    {
+            //        var body = reader.Read(bodyLength);
+            //        receiveQueue.Enqueue(body);
+            //    }
+            //}
+
+            receiveQueue.Enqueue(reader.Read(reader.Length));
+        }
+        public void Pack(Queue<byte[]> sendQueue, IByteArray writer)
+        {
+            //add head length or id
+            //byte[] head = new Byte[1] { 4 };
+            //writer.Write(head, head.Length);
+            //var body = sendQueue.Dequeue();
+
+            var test = sendQueue.Dequeue();
+            writer.Write(test, test.Length);
+        }
+    }
+}
+    
---
+++ Assets/Assets/HiSocket/HINetworkData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HiSocket
+{
+    [Serializable]
+    public class HINetworkItem
+    {
+        public int ID = 0;
+        public string Name = "";
+        public bool IsEnable = false;
+    }
+
+    [Serializable]
+    public class HINetworkData
+    {
+        public Dictionary<string, HINetworkItem> ndada = new Dictionary<string, HINetworkItem>();
+    }
+}
